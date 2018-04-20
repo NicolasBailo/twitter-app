14da93a3b432bbe1ab7869c3d03a7f8df7297f86
NicolasBailo <>
2018-04-20 15:49:39 +0200

    Rendering encrypted tweets

---
+++ src/main/resources/static/js/functions.js
@@ -0,0 +1,22 @@
+function registerSearch() {
+    $("#search").submit(function(event){
+        event.preventDefault();
+        var target = $(this).attr('action');
+        var query = $("#q").val();
+        $.get(target, { q: query } )
+            .done( function(data) {
+                $("#resultsBlock").empty().append(data);
+                var template = $('#template').html();
+                Mustache.parse(template);   // optional, speeds up future uses
+                var rendered = Mustache.render(template,{tweets: data});
+                $('#resultsBlock').html(rendered);
+            }).fail(function() {
+            $("#resultsBlock").empty();
+        });
+    });
+}
+
+$(document).ready(function() {
+    registerSearch();
+});
+
